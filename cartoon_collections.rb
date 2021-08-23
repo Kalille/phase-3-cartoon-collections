c189b4e76c39d422c2e8286bcf9a8fed964481e8
kalille Overton <>
2021-08-23 16:32:37 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,35 @@
-def roll_call_dwarves# code an argument here
+require 'pry'
+
+def roll_call_dwarves(dwarves)# code an argument here
+  dwarves.each.with_index(1) do |dwarf,index|
+    puts  "#{index} #{dwarf}"
+  end
+
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+ caps = array.map(&:capitalize)
+ caps.map{|element| element + "!"}
+  
+
+  
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
-end
+  array.any?{|element|
+   element.length > 4}
+     
+    
+  end
 
-def find_the_cheese# code an argument here
+
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.include?(cheese_types)
+
 end
+
